438fe4b74042cb4a59817d9e86785f02c7dac51a
Solutionsafoot <112793940+Solutionsafoot@users.noreply.github.com>
2024-01-09 13:23:43 -0500

    try c#

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: dotnet HerokuTest.dll
---
+++ Startup.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+
+namespace HerokuTest
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add any services if needed
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.Run(async (context) =>
+            {
+                // Log to the console
+                Console.WriteLine("Console App Executed!");
+                
+                // Respond to the HTTP request
+                await context.Response.WriteAsync("Console App Executed!");
+            });
+        }
+    }
+}
